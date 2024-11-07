c95f4fa32eab3eedb58348d0fc1e3bf9d7a51d1a
Francesco Pio Ruffo <122920334+FPR10@users.noreply.github.com>
2024-11-07 11:36:03 +0100

    Update dowload_file_sequenza.py

--- dowload_file_sequenza.py
+++ dowload_file_sequenza.py
@@ -5,8 +5,8 @@ from Bio import SeqIO
 url = 'https://siloe.dimes.unical.it/~fab/MedicinaTD/group_1_seq.fasta'
 filename = wget.download(url)
 
-#Stampa della lunghezza della sequenza
+# Apertura e calcolo della lunghezza della sequenza
 with open(filename, "r") as file:
     for record in SeqIO.parse(file, "fasta"):
         sequence_length = len(record.seq)
-        print("Lunghezza della sequenza:", sequence_length)
+        print("Lunghezza della sequenza:", f"{sequence_length:,}")
