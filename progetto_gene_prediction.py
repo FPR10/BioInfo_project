3b568f57bd9dd8e19a9ed4faeba3fd3d1f51a370
EndriuRox <161837278+EndriuRox@users.noreply.github.com>
2024-11-09 20:13:58 +0100

    Update progetto_gene_prediction.py

--- progetto_gene_prediction.py
+++ progetto_gene_prediction.py
@@ -1,5 +1,5 @@
 #####################################################
-''' 
+'''
 Identificativo del gruppo
 group_id = 1
 
@@ -13,11 +13,14 @@ Partecipanti
 # Importazione moduli
 import numpy as np
 import pandas as pd
+import os
 import regex
 import Bio
 import Bio.SeqIO
 from bisect import bisect_left
 import pickle
+import Bio.Blast
+from Bio.Blast import NCBIWWW, NCBIXML
 
 
 # RICERCA DELLE ORF
@@ -25,34 +28,38 @@ import pickle
 # le modalità d'uso sono equivalenti al find_iter di regex
 
 class Match:
-	def __init__(self, string, start, stop):
-		self.string = string
-		self.start_pos = start
-		self.stop_pos = stop
-		self._group0 = string[start:stop]
-	def group(self):
-		return self._group0
-	def span(self):
-		return (self.start_pos, self.stop_pos)
-	def __repr__(self):
-		return f"<Match object; span={self.span()}, match='{self.group()}'>"
+    def __init__(self, string, start, stop):
+        self.string = string
+        self.start_pos = start
+        self.stop_pos = stop
+        self._group0 = string[start:stop]
+
+    def group(self):
+        return self._group0
+
+    def span(self):
+        return (self.start_pos, self.stop_pos)
+
+    def __repr__(self):
+        return f"<Match object; span={self.span()}, match='{self.group()}'>"
+
 
 def orf_iter(sequence):
-	# Cerco tutti i codoni di stop e salvo il punto di partenza
-	stops = []
-	for stop in regex.finditer(r"TAA|TAG|TGA", sequence):
-		stops.append(stop.span()[0])
-	# Itero sugli start
-	for start in regex.finditer(r"ATG", sequence):
-		# cerco il primo stop con valore di distanza multiplo di 3
-		pos_start = start.span()[0]
-		pos_stop = bisect_left(stops, pos_start + 3)
-		while (pos_stop < len(stops)) and ((stops[pos_stop] - pos_start) % 3 != 0):
-			pos_stop = pos_stop + 1
-		if pos_stop < len(stops):
-			yield Match(sequence, pos_start, stops[pos_stop])
-   
-   
+    # Cerco tutti i codoni di stop e salvo il punto di partenza
+    stops = []
+    for stop in regex.finditer(r"TAA|TAG|TGA", sequence):
+        stops.append(stop.span()[0])
+    # Itero sugli start
+    for start in regex.finditer(r"ATG", sequence):
+        # cerco il primo stop con valore di distanza multiplo di 3
+        pos_start = start.span()[0]
+        pos_stop = bisect_left(stops, pos_start + 3)
+        while (pos_stop < len(stops)) and ((stops[pos_stop] - pos_start) % 3 != 0):
+            pos_stop = pos_stop + 1
+        if pos_stop < len(stops):
+            yield Match(sequence, pos_start, stops[pos_stop])
+
+
 ################################################################################
 #                         ALGORITMO DI GENE PREDICTION                         #
 # Scopo del progetto è, data la sequenza scaricata, salvare in un dataframe    #
@@ -68,40 +75,32 @@ seq = next(Bio.SeqIO.parse(f"group_1_seq.fasta", "fasta")).seq
 #
 # PREPARAZIONE DEL DATAFRAME CON I RISULTATI
 #
-results = pd.DataFrame(columns=["ORFpos","Coding","BLAST"])
+results = pd.DataFrame(columns=["ORFpos", "Coding", "BLAST"])
 # per aggiungere una riga in posizione i, inizializzando a False i campi:
 # results.loc[i] = [orf.span(), False, False]
 # per modificare ad esempio il campo Coding della riga i a True:
 # results.loc[i, 'Coding'] = True
 
-#
-# SCANSIONE DELLE ORF NELLO STRAND DIRETTO
-#
-for orf in orf_iter(str(seq)):
-  # FILTRAGGIO PRELIMINARE per lunghezza (>150): se la lunghezza della ORF è minore della soglia non la consideriamo
-  #altrimenti la aggiunge al risultato
-  lunghezza = orf.span()[1] - orf.span()[0]
-  if lunghezza > 250:
-	  results.loc[len(results)] = [orf.span()[0], False, False]
-pass
-
-
-# SALVATAGGIO DEI RISULTATI SU FILE
-with open(f"group_{group_id}_results.pickle", "wb") as f:
-	pickle.dump(results, f)
-
-
-###########################################################################################################################################################################################
-
-# INSERIRE QUI LE PROPRIE RIGHE DI CODICE
+########################################################################################################################
+#DICHIARAZIONE FUNZIONI UTILI
 
 # SENSORI DI SEGNALE
-def tata_box(sequenza):
+def tata_box(sequenza, posA):
     regex_tata = r"(TATA)(A|T)(A)(A|T)"
-    ret = regex.search(regex_tata, sequenza)
-    return ret.span()
+    substring = sequenza[posA-35:posA-2]
+    ret = regex.search(regex_tata, substring)
+    if ret is not None:
+        return True
+    return False
 
 def iniziatore(sequenza):
+    regex_in = r"(C|T){2}A(A|C|G|T)(A|T)(C|T){2}"
+    ret = regex.search(regex_in, sequenza)
+    if ret is not None:
+        return True
+    return False
+
+def posizione_iniziatore(sequenza):
     regex_in = r"(C|T){2}A(A|C|G|T)(A|T)(C|T){2}"
     ret = regex.search(regex_in, sequenza)
     return ret.span()
@@ -109,7 +108,9 @@ def iniziatore(sequenza):
 def sequenza_Kozak(sequenza):
     regex_K = r"(GCC)?(GCC)(A|G)(CC)(ATG)(G)"
     ret = regex.search(regex_K, sequenza)
-    return ret.span()
+    if ret is not None:
+        return True
+    return False
 
 # SENSORI DI CONTENUTO
 def cg_content(sequenza):
@@ -138,24 +139,82 @@ def isole_CpG(sequenza):
     # CONTENUTO CG
     contenuto_GC = cg_content(sequenza)
 
-    return CpG_osservato_atteso, contenuto_GC
+    if contenuto_GC >= 0.5 and CpG_osservato_atteso >= 0.6:
+        return True
+    return False
 
     # NB. CONTENUTO CG > 0.5; RAPPORTO CpG OSSERVATO/ATTESO > 0.6
 
-# PROVE
-# gen = orf_iter(str(seq))
-# print(next(gen))
-# print(next(gen))
 
-# print(results)
-prova = "TATAAATCGTAATCTAGTCC"
-print(iniziatore(str(prova)))
+# FUNZIONE PER LA RICERCA BLAST
 
-prova_CpG = "ATGCGATACGCGTAA"
-print(isole_CpG(prova_CpG))
+def ricerca_blast(sequenza):
+    res = []
+    # Esegui la ricerca BLAST
+    x = NCBIWWW.qblast('blastn', 'nt', sequenza, service='megablast')
 
-prova_K1 = "CATGCCGCCGCCATGGTTT"
-print(sequenza_Kozak(prova_K1))
+    # Parla direttamente la risposta senza salvare su file
+    blast_records = NCBIXML.parse(x)
 
-prova_K2 = "ATCGCCACCATGGATG"
-print(sequenza_Kozak(prova_K2))
+     # Itera sui risultati e raccogli i dati principali
+    for blast_record in blast_records:
+        for alignment in blast_record.alignments:
+            for hsp in alignment.hsps:
+                res.append({
+                    "Sequenza considerata": seq,
+                    "Nome allineamento": alignment.title,
+                    "Lunghezza allineamento": alignment.length,
+                    "E-value": hsp.expect,
+                    "Score": hsp.score,
+                    "Inizio query": hsp.query_start,
+                    "Fine query": hsp.query_end,
+                })
+    x.close()
+    return res
+
+
+# FUNZIONE PER COSTRUIRE IL DATAFRAME CON I DATI DI BLAST
+
+def costruisci_dataFrame_risultati_blast(sequenza):
+    df = pd.DataFrame(ricerca_blast(sequenza))
+    return df
+
+########################################################################################################################
+
+#DIZIONARIO PER MEMORIZZARE LE ORF E I RELATIVI PUNTEGGI
+
+raccolta_orf = {}
+
+for orf in orf_iter(str(seq)):
+    # FILTRAGGIO PRELIMINARE per lunghezza (>250): se la lunghezza della ORF è minore della soglia non la consideriamo
+    # altrimenti la aggiunge al risultato
+    lunghezza = orf.span()[1] - orf.span()[0]
+    if lunghezza > 250:
+        raccolta_orf[orf] = 0
+        if sequenza_Kozak((str)(orf)):
+            raccolta_orf[orf] += 1
+        if iniziatore((str)(seq[:orf.span()[0]])):
+            raccolta_orf[orf] += 1
+            pos = posizione_iniziatore((str)(seq[:orf.span()[0]]))[0]+2
+            if tata_box((str)(seq), pos):
+                raccolta_orf[orf] +=1
+        if isole_CpG((str)(orf)):
+            raccolta_orf[orf] += 1
+
+#FILTRAGGIO ORF CON PUNTEGGIO PIÙ ALTO E INSERIMENTO NEL DATAFRAME FINALE
+
+for orf in raccolta_orf:
+    if raccolta_orf[orf] > 1:
+        results.loc[len(results)] = [orf.span()[0], True, False] #SE HANNO UN PUNTEGGIO > 1, SUPPONIAMO CHE SIANO CODIFICANTI
+        if not costruisci_dataFrame_risultati_blast(orf.group()).empty:
+            results.loc[len(results)-1, 'BLAST'] = True #SE SI TROVANO CORRISPONDENZE SU BLAST, VIENE AGGIORNATO IL PARAMETRO
+
+# SALVATAGGIO DEI RISULTATI SU FILE
+with open(f"group_1_results.pickle", "wb") as f:
+    pickle.dump(results, f)
+
+
+###########################################################################################################################################################################################
+
+# INSERIRE QUI LE PROPRIE RIGHE DI CODICE
+print(results)
