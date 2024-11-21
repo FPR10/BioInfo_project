1a79d880af56d88389d0dbe0513f0a649234483c
Francesco Pio Ruffo <122920334+FPR10@users.noreply.github.com>
2024-11-22 00:24:47 +0100

    Add files via upload

---
+++ Entropia.py
@@ -0,0 +1,41 @@
+import math
+
+def contaOcc(x, stringa):
+    return stringa.count(x)
+
+
+#Calcolo della probabilità dell'evento
+def prob_carattere_singolo(carattere, seq):
+    return contaOcc(carattere, seq)/len(seq)
+
+
+def prob_seq_nucleotidica (seq):
+    letters = ['A','T','C','G']
+    ret = []
+    for i in letters:
+        ret.append (prob_carattere_singolo(i,seq))
+    return ret # ret = [P(A), P(T), P(C), P(G)]
+        
+        
+#devo passare come parametro l'output del metodo "prob_seq_nucleotidica"
+def contInformativo (arrayProb):
+    ret = [] # ret = [I(A), I(T), I(C), I (G)]
+    for elem in arrayProb:
+        ret.append (-math.log(elem,2))
+    return ret
+
+
+#Riceve la sequenza e calcola arrayProb e arrayContInfo internamente. Possiamo così invocare il metodo "entropia" direttamente sulla orf
+#senza preoccuparci di effettuare operazioni di calcolo preliminari
+def calcola_entropia (sequenza):
+    arrayProb = prob_seq_nucleotidica(sequenza)
+    arrayContInfo = contInformativo (arrayProb)
+    res = 0
+    for i in range (len (arrayProb)):
+        for j in range (len (arrayContInfo)):
+            if i == j:
+                res += arrayProb[i]*arrayContInfo[j]
+    out = str(res) 
+    return out
+
+
